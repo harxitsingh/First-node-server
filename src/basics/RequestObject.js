59b5bfb8498a570c3f7378d6b440117e9a792bc2
Harshit Singh <harxitsingh@gmail.com>
2026-03-20 22:05:59 +0530

    Update RequestObject.js

--- src/basics/RequestObject.js
+++ src/basics/RequestObject.js
@@ -3,6 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res)  =>  {
   console.log(req.url, req.method, req.headers); // request object
 
+  
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title> Complete Coding </title></head>')
